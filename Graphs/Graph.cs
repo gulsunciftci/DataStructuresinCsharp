d4e0200e88aa0df4d72510cbb8dc6eb68d883f8d
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-06-11 23:49:46 +0300

    graph kodlarını yazdım

---
+++ Graphs/Graph.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class Graph
+    {
+        private int Vertex;
+        private List<Int32>[] adjacecny;
+
+        public Graph(int v)
+        {
+            Vertex = v;
+            adjacecny = new List<int>[v];
+            for (int i=0;i<v;i++)
+            {
+                adjacecny[i] = new List<int>();
+            }
+        }
+        public void AddEdge(int v,int w)
+        {
+            adjacecny[v].Add(w);
+        }
+        public void BFS(int s)
+        {
+            bool[] visited = new bool[Vertex];
+            Queue<int> queue = new Queue<int>();
+            visited[s] = true;
+            queue.Enqueue(s);
+
+            while (queue.Count != 0)
+            {
+                s = queue.Dequeue();
+                Console.WriteLine("next-->" + s);
+
+                foreach (int next in adjacecny[s])
+                {
+                    if (!visited[next])
+                    {
+                        visited[next] = true;
+                        queue.Enqueue(next);
+                    }
+                }
+            }
+        }
+        public void DFS(int s)
+        {
+            bool[] visited = new bool[Vertex];
+            Stack<int> stack = new Stack<int>();
+            visited[s] = true;
+            stack.Push(s);
+
+            while (stack.Count != 0)
+            {
+                s = stack.Pop();
+                Console.WriteLine("next-->" + s);
+
+                foreach (int next in adjacecny[s])
+                {
+                    if (!visited[next])
+                    {
+                        visited[next] = true;
+                        stack.Push(next);
+                    }
+                }
+            }
+        }
+        public void PrintAdjacecnyMatrix()
+        {
+            for (int i=0;i<Vertex;i++)
+            {
+                string s = "";
+                foreach (var k in adjacecny[i])
+                {
+                    s = s + (k + ",");
+                }
+                s = s.Substring(0, s.Length - 1);
+                s =s+"]";
+                Console.WriteLine(i+":["+s);
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ Graphs/Program.cs
@@ -0,0 +1,26 @@
+﻿using Graphs;
+
+namespace DataStructuresinCharp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Graph graph = new Graph(4);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 3);
+
+            graph.PrintAdjacecnyMatrix();
+
+            Console.WriteLine("BFS traversel starting from vertex 1:");
+            graph.BFS(1);
+            Console.WriteLine("DFS traversel starting from vertex 1:");
+            graph.DFS(1);
+        }
+    }
+}

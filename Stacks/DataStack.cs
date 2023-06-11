acc5a6bb4879c999fb07fa06fe86949c5d4849e7
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-06-11 18:14:55 +0300

    stack kodlarını yazdım

---
+++ Stacks/DataStack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    public class DataStack
+    {
+        public Node top;
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+        public int Peek()
+        {
+            if (top != null)
+            {
+                return top.data;
+            }
+            return -1;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            node.next = top;
+            top = node;
+        }
+        public int Pop()
+        {
+            if (top != null)
+            {
+                int data = top.data;
+                top = top.next;
+                return data;
+            }
+            return -1;
+        }
+        public void printList()
+        {
+            Node n = this.top;
+            while (n != null)
+            {
+
+                Console.Write(n.data + " ");
+                n = n.next;
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ Stacks/Program.cs
@@ -0,0 +1,24 @@
+﻿using Stacks;
+using System;
+using System.Collections;
+
+namespace DataStructuresinCharp
+{
+    public class Stacks
+    {
+        public static void Main()
+        {
+
+            DataStack s = new DataStack();
+            s.Push(0);
+            s.Push(8);
+            s.Push(11);
+            s.Push(9);
+            s.Pop();
+
+            s.printList();
+            Console.WriteLine(s.Peek());
+            Console.WriteLine(s.IsEmpty());
+        }
+    }
+}
